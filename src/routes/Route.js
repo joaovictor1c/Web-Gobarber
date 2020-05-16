ff130c7f028c50243e7ae7028a44546810a21646
joao victor <joaovictor1c2013@gmail.com>
2020-05-16 11:21:35 -0300

    base do projeto

---
+++ src/routes/Route.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import ProtoTypes from 'prop-types';
+
+import AuthLayout from '../pages/_layouts/auth';
+import DefaultLayout from '../pages/_layouts/default';
+
+export default function RoutesWrapper({
+  component: Component,
+  isPrivate,
+  ...rest
+}) {
+  const signed = false;
+
+  if (!signed && isPrivate) {
+    return <Redirect to="/" />;
+  }
+
+  if (signed && isPrivate) {
+    return <Redirect to="/dashboard" />;
+  }
+
+  const Layout = signed ? DefaultLayout : AuthLayout;
+  return (
+    <Route
+      {...rest}
+      render={(props) => (
+        <Layout>
+          <Component {...props} />
+        </Layout>
+      )}
+    />
+  );
+}
+
+RoutesWrapper.protoTypes = {
+  isPrivate: ProtoTypes.bool,
+  component: ProtoTypes.oneOfType([ProtoTypes.element, ProtoTypes.func])
+    .isRequired,
+};
+
+RoutesWrapper.defaultProps = {
+  isPrivate: false,
+};
